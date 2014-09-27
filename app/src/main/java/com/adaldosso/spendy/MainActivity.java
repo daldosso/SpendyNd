067f01bdded08331b1985c5774ece19e28a8d769
Alberto Dal Dosso <daldosso@gmail.com>
2014-09-27 18:10:40 +0200

    Create an http post

--- app/src/main/java/com/adaldosso/spendy/MainActivity.java
+++ app/src/main/java/com/adaldosso/spendy/MainActivity.java
@@ -5,17 +5,23 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -37,23 +43,27 @@ public class MainActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
     public void login(View view) throws IOException {
-        HttpClient httpclient = new DefaultHttpClient();
+        EditText editEmail = (EditText) findViewById(R.id.editEmail);
+        EditText editPassword = (EditText) findViewById(R.id.editPassword);
+        HttpClient httpClient = new DefaultHttpClient();
         URL url = new URL("");
         HttpResponse response;
-        response = httpclient.execute(new HttpGet(String.valueOf(url)));
+        HttpPost httpPost = new HttpPost(String.valueOf(url));
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+        nameValuePairs.add(new BasicNameValuePair("username", editEmail.getText().toString()));
+        nameValuePairs.add(new BasicNameValuePair("password", editPassword.getText().toString()));
+        httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        response = httpClient.execute(httpPost);
         StatusLine statusLine = response.getStatusLine();
         if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             response.getEntity().writeTo(out);
             out.close();
-            String responseString = out.toString();
+            //String responseString = out.toString();
         } else {
             response.getEntity().getContent().close();
             throw new IOException(statusLine.getReasonPhrase());
