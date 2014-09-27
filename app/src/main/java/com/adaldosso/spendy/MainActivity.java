8c3affbb32bace60f23cf939357852c3b31c9266
Alberto Dal Dosso <daldosso@gmail.com>
2014-09-27 20:49:39 +0200

    Added sign in

--- app/src/main/java/com/adaldosso/spendy/MainActivity.java
+++ app/src/main/java/com/adaldosso/spendy/MainActivity.java
@@ -2,6 +2,7 @@ package com.adaldosso.spendy;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +17,8 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -26,10 +29,16 @@ import java.util.List;
 
 public class MainActivity extends Activity {
 
+    public static final String LOGIN_URL = "http://www.adaldosso.com/quantospendi/srv/login.php";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
     }
 
 
@@ -46,11 +55,11 @@ public class MainActivity extends Activity {
         return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
-    public void login(View view) throws IOException {
+    public void login(View view) throws IOException, JSONException {
         EditText editEmail = (EditText) findViewById(R.id.editEmail);
         EditText editPassword = (EditText) findViewById(R.id.editPassword);
         HttpClient httpClient = new DefaultHttpClient();
-        URL url = new URL("");
+        URL url = new URL(LOGIN_URL);
         HttpResponse response;
         HttpPost httpPost = new HttpPost(String.valueOf(url));
         List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
@@ -63,7 +72,13 @@ public class MainActivity extends Activity {
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             response.getEntity().writeTo(out);
             out.close();
-            //String responseString = out.toString();
+            String responseString = out.toString();
+            JSONObject json = new JSONObject(responseString);
+            if (json.getBoolean("success")) {
+                Utils.showMessage(this, "Connesso");
+            } else {
+                Utils.showMessage(this, "Non Connesso");
+            }
         } else {
             response.getEntity().getContent().close();
             throw new IOException(statusLine.getReasonPhrase());
