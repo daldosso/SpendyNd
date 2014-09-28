fa332c4f8b1d10ed1d7ae45fa611a01c37b1a8d9
Alberto Dal Dosso <daldosso@gmail.com>
2014-09-28 18:03:12 +0200

    Started registration

---
+++ app/src/main/java/com/adaldosso/spendy/RegistrationFragment.java
@@ -0,0 +1,16 @@
+package com.adaldosso.spendy;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RegistrationFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_registration, container, false);
+        return rootView;
+    }
+}
